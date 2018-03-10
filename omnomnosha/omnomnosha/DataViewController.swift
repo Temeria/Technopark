893da73649bf33e84d49e0e15c4a805d5fc9fef1
Дмитрий Гурьянов <givy11@gmail.com>
2018-03-10 17:43:28 +0300

    Basic template from XCode added

---
+++ omnomnosha/omnomnosha/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  omnomnosha
+//
+//  Created by Дмитрий Гурьянов on 10.03.2018.
+//  Copyright © 2018 Temeria. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
